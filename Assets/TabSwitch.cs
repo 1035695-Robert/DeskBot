0ac1de37bafda9c739545bbab7c296a720188c8d
Robert lab303 <1035695@student.sae.edu.au>
2026-07-28 18:56:29 +1000

    Toggle between UI tabs

--- Assets/TabSwitch.cs
+++ Assets/TabSwitch.cs
@@ -1,9 +1,36 @@
 using UnityEngine;
 using UnityEngine.UI;
 
-public class TabSwitch: MonoBehaviour
+public class TabSwitch : MonoBehaviour
 {
+   public Button keyboardButton;
+    public Button unlockables;
+    
+    [SerializeField] private GameObject keyboardUi;
+    [SerializeField] private GameObject unlockableUi;
+    void OnEnable()
+    {
+        unlockables.onClick.AddListener(OpenUnlockableAbilityTree);
+        keyboardButton.onClick.AddListener(OpenKeyboardUI);
+        if(keyboardUi.activeSelf)
+            OpenKeyboardUI();
+        else if(unlockableUi.activeSelf)
+            OpenUnlockableAbilityTree();
+    }
 
-public Button AbilitiesButton
+    private void OpenKeyboardUI()
+    {
+        keyboardUi.SetActive(true);
+        keyboardButton.interactable = false;
+        unlockableUi.SetActive(false);
+        unlockables.interactable = true;
+    }
 
+    private void OpenUnlockableAbilityTree()
+    {
+        keyboardUi.SetActive(false);
+        keyboardButton.interactable = true;
+        unlockableUi.SetActive(true);
+        unlockables.interactable = false;
+    }
 }
